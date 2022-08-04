e3608aa14f47f5fe187387ea252178b2e7c851c5
emaatkcld <98997609+emaatkcld@users.noreply.github.com>
2022-08-04 18:58:29 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
  terraform {
    backend "s3" {
-    bucket = "code-sharing-bucket"
+    bucket = "jdfhdhfdf2546"
     key    = "avinash/s3file-3.tfstate"
     region = "us-west-2"
     profile = "jespo"
-dynamodb= "firstprojecttable"
+dynamodb= "terraformtable"
    }
  }
 
