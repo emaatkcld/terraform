112c1da5a245a41c74cb39bbb41d0d3cb7dbcdef
emaatkcld <98997609+emaatkcld@users.noreply.github.com>
2022-08-04 19:01:51 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
  terraform {
    backend "s3" {
     bucket = "jdfhdhfdf2546"
-    key    = "avinash/s3file-3.tfstate"
+    key    = "dre/s3file-3.tfstate"
     region = "us-west-2"
     profile = "jespo"
 dynamodb= "terraformtable"
